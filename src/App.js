ff5871a5d5a9d88fe5f195c57310cb280452bc03
Nathan Farley <nathanfarley@Nathans-MBP.lan>
2022-07-01 19:25:18 -0400

    completed Detail components and setup React-router.

--- src/App.js
+++ src/App.js
@@ -3,14 +3,22 @@ import { Counter } from './features/counter/Counter';
 import './App.css';
 import Header from './components/Header/header';
 import Home from './components/Home/home';
+import { Routes, BrowserRouter as Router, Route, Link, BrowserRouter } from "react-router-dom";
+import Detail from './components/Detail/Detail';
 
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Home />
-      
+    <div className="App"> 
+    
+         <Router>
+              <Header />
+              <Routes>
+                  <Route exact path="detail" element={<Detail />} />
+                  <Route exact path="/" element={<Home />} />
+              </Routes>
+          </Router>
+          
     </div>
   );
 }
